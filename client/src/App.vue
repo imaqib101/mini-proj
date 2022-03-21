c6e489af37543aa333337f1c49df407a99bb3155
Mohammed Aqib <62829012+imaqib101@users.noreply.github.com>
2022-03-21 09:03:35 +0530

    Add files via upload

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main> <NavBar /><FileUpload /> </v-main>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import FileUpload from "./components/FileUpload.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    NavBar,
+    FileUpload,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/components/FileUpload.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-file-input counter show-size truncate-length="15"></v-file-input>
+  <v-btn color="primary" elevation="3" x-large>Scrape</v-btn>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
+
+<style></style>
